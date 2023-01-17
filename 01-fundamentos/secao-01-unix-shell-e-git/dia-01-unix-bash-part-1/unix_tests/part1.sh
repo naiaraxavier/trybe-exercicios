060c0d46c5df850f8a24284651e7d3e6bec22080
Naiara Ferreira Xavier da Silva Martins <naiaraxf@gmail.com>
2023-01-17 20:08:12 -0300

    Primeira parte do exercicício

---
+++ 01-fundamentos/secao-01-unix-shell-e-git/dia-01-unix-bash-part-1/unix_tests/part1.sh
@@ -0,0 +1,37 @@
+  627  ls
+  628  mkdir unix_tests
+  629  ls
+  630  cd unix_tests
+  631  touch trybe.txt
+  632  ls
+  633  cp trybe.txt trybe_backup.txt
+  634  ls
+  635  mv trybe.txt trybe-renomeado.txt
+  636  ls
+  637  cd ..
+  638  ls
+  639  cd unix_tests
+  640  ls
+  641  mkdir backup
+  642  ls
+  643  mv trybe_backup.txt backup/
+  644  ls
+  645  cd backup
+  646  ls
+  647  cd ..
+  648  mkdir backup2
+  649  ls
+  650  mv ./backup/trybe_backup.txt backup2/
+  651  cd backup2
+  652  ls
+  653  cd ..
+  654  ls
+  655  rmdir backup
+  656  ls
+  657  mv backup2 backup/
+  658  ls
+  659  pwd
+  660  ls -l
+  661  rm -rf backup
+  662  ls
+  663  clear

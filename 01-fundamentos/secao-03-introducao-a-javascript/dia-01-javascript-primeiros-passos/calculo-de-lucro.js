07dabbd5410cbe1c69952b6f9b5e97a59889133c
Naiara Ferreira Xavier da Silva Martins <naiaraxf@gmail.com>
2023-01-24 21:21:45 -0300

    Exercício 9 concluído

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/calculo-de-lucro.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/calculo-de-lucro.js
@@ -1,8 +1,14 @@
-//9- Calcular o lucro:
-let valorCusto = 100 * (20/100) + 100;
-console.log(valorCusto);
+// 9 - Calcular o valor do lucro, considerando que há incidencia de 20% de imposto sobre o valor de custo e foram vendidos 1000 produtos
 
-const valorVenda = 150;
-let qtadeVendidos = 1000;
+const valorCusto = 8;
+const valorVenda = 15;
 
-let valorLucro = (valorVenda - valorCusto) * qtadeVendidos;
+
+if(valorCusto > 0 && valorVenda > 0){
+  const impostoSobreCusto = valorCusto * 0.2;
+  const valorCustoTotal = valorCusto + impostoSobreCusto;
+  const lucro = (valorVenda - valorCustoTotal) * 1000;
+  console.log("Valor do lucro é: "+lucro+" reais");
+}else {
+  console.log ("Valor inválido");
+}

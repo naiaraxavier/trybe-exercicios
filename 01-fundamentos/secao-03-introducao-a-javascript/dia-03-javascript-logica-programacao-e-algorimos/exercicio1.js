034c2a17edf3d6f8be659817fc71a00bc93605ec
Naiara Ferreira Xavier da Silva Martins <naiaraxf@gmail.com>
2023-01-26 01:13:19 -0300

    Exercício 2 concluído

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-programacao-e-algorimos/exercicio1.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-programacao-e-algorimos/exercicio1.js
@@ -1,3 +1,4 @@
+//1 - Criar um algoritmo que imprima o fatorial de 10
 
 const number = 4;
 let fatorial = 1;
@@ -7,5 +8,15 @@ for(let index = number; index > 1; index -= 1){
 }
 
 console.log(fatorial);
+
+//2 - Utilizar um for para inverter uma palavra
+
+let word = 'naiara';
+let inverted = "";
+
+  for(let index = word.length; index > 0 ; index -= 1){
+    inverted += word[index -1];
+  }
+  console.log(inverted);
 
 

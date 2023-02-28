29900c2eaf91e83974e4710bca960dfe2a0dddff
Naiara Martins <naiaraxf@gmail.com>
2023-02-28 12:30:09 -0300

    exercícios 1

---
+++ 01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-1.test.js
@@ -0,0 +1,10 @@
+const myRemove = require('./exercicio-1.js');
+
+describe('The function myRemove', () => {
+  it('Retorna um array removendo o item correto', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
+

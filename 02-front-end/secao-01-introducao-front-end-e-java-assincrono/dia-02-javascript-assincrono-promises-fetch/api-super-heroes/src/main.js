3c9a84f35c3ada5a9efd63a655efa9d2ad8a0710
Naiara Martins <naiaraxf@gmail.com>
2023-03-17 22:26:29 -0300

    API de Super Heróis

--- 02-front-end/secao-01-introducao-front-end-e-java-assincrono/dia-02-javascript-assincrono-promises-fetch/api-super-heroes/src/main.js
+++ 02-front-end/secao-01-introducao-front-end-e-java-assincrono/dia-02-javascript-assincrono-promises-fetch/api-super-heroes/src/main.js
@@ -6,11 +6,9 @@ const allHeroes = () => {
   const name = document.getElementById('name');
   const number = 731;
   const idRandom = Math.round(Math.random() * number);
-  // console.log(idRandom);
   fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${idRandom}.json`)
     .then((response) => response.json())
     .then((data) => {
-      // console.log(data);
       image.src = data.images.sm;
       name.innerText = data.name;
     })
